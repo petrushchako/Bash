dc94b83d5e7d76c8570e46dc7c1619d38a97d70d
Alex Petrushchak <petrushchako@gmail.com>
2024-01-06 22:21:44 +0000

    Adjust backup.sh script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -3,18 +3,18 @@
 # Using Bash to write backup script
 # Write the Script to Back Up the Requested Files
 
+LOGFILE=/home/cloud_user/backup_logs
+
 # Create log entry and direcotry
 echo "Backup starting"
-echo "Creating backup directory" >> /home/cloud_user/backup_logs
-mkdir /home/cloud_user/work_backup 2> /dev/null || echo "Directory work_backup e
-xists"
+echo "Creating backup directory" >> $LOGFILE
+mkdir /home/cloud_user/work_backup 2> /dev/null || echo "Directory work_backup exists"
 
 
 # Copying files
-echo "Backing up files" >> /home/cloud_user/backup_logs
-cp -v /home/cloud_user/work/* /home/cloud_user/work_backup/ >> /home/cloud_user/
-backup_logs
-echo "Finished Copying Files" >> /home/cloud_user/backup_logs
+echo "Backing up files" >> $LOGFILE
+cp -v /home/cloud_user/work/* /home/cloud_user/work_backup/ >> $LOGFILE
+echo "Finished Copying Files" >> $LOGFILE
 
 echo "Backup completed"
 
