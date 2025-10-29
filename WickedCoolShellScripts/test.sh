c8d2ef4ef0ed23b0dbdf3af0f7f617cffe08547b
petrushchako <petrushchako@gmail.com>
2025-10-29 22:43:59 +0000

    Add Arrays content

--- WickedCoolShellScripts/test.sh
+++ WickedCoolShellScripts/test.sh
@@ -4,6 +4,18 @@ for i in $@;do
     echo $i
 done
 
-message=("Hello" "World")
+message=("Hello" "  " "World" "\n")
+indexes=${!message[@]}
 
-echo $message[@]
+for i in {0..3}; do
+    echo -ne "${message[i]}"
+done
+
+echo $indexes
+echo {0..3}
+
+if [ $indexes -eq {0..3} ]; then
+    echo "Equal"
+else
+    echo "not Equal"
+fi
